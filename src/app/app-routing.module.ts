2510f177b323643f2e3eed56dcac228103343b01
Leonardo <leowitcroz@gmail.com>
2022-10-13 23:57:05 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { FrenchComponent } from './paginas/french/french.component';
+import { InglesComponent } from './paginas/ingles/ingles.component';
+import { PortuguesComponent } from './paginas/portugues/portugues.component';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PaginaInicialComponent},
+  {path:'pt-br',component:PortuguesComponent},
+  {path:'ing',component:InglesComponent},
+  {path:'french',component:FrenchComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
+import { PortuguesComponent } from './paginas/portugues/portugues.component';
+import { InglesComponent } from './paginas/ingles/ingles.component';
+import { FrenchComponent } from './paginas/french/french.component';
+import { SobreComponent } from './componentes/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PaginaInicialComponent,
+    PortuguesComponent,
+    InglesComponent,
+    FrenchComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
